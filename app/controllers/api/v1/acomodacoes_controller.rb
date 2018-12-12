c6b837044b53501967e5cee7523d156d8b9cc72c
bcnogueira <bcnogueira@id.uff.br>
2018-12-12 01:39:43 -0200

    Fazendo a api funcionar.

--- app/controllers/api/v1/acomodacoes_controller.rb
+++ app/controllers/api/v1/acomodacoes_controller.rb
@@ -1,59 +1,70 @@
 module Api::V1
   class AcomodacoesController < ApplicationController
-    before_action :set_acomodacao, only: [:show, :update, :destroy]
+    # before_action :set_acomodacao, only: [:show, :update, :destroy]
 
     # GET /acomodacoes
     def index
       @acomodacoes = Acomodacao.all
-
       render json: @acomodacoes
     end
 
     # GET /acomodacoes/1
     def show
-      render json: @acomodacao
+      @acomodacao = Acomodacao.find(params[:id])
+      # binding.pry
+      # file.service_url
+      teste = {}
+      teste = @acomodacao.files.map do |file|
+        file.service_url
+      end
+      teste2 = { urls: teste }
+      # binding.pry
+
+      # render json: @acomodacao, testando: teste.to_json
+
+      render json: { acomodacao: @acomodacao, urls: teste }
     end
 
     # POST /acomodacoes
-    def create
-      @acomodacao = Acomodacao.new(acomodacao_params)
-
-      if @acomodacao.save
-        render json: @acomodacao, status: :created
-      else
-        render json: @acomodacao.errors, status: :unprocessable_entity
-      end
-    end
+    # def create
+    #   @acomodacao = Acomodacao.new(acomodacao_params)
+    #
+    #   if @acomodacao.save
+    #     render json: @acomodacao, status: :created
+    #   else
+    #     render json: @acomodacao.errors, status: :unprocessable_entity
+    #   end
+    # end
 
     # PATCH/PUT /acomodacoes/1
-    def update
-      if @acomodacao.update(acomodacao_params)
-        render json: @acomodacao
-      else
-        render json: @acomodacao.errors, status: :unprocessable_entity
-      end
-    end
+    # def update
+    #   if @acomodacao.update(acomodacao_params)
+    #     render json: @acomodacao
+    #   else
+    #     render json: @acomodacao.errors, status: :unprocessable_entity
+    #   end
+    # end
 
     # DELETE /acomodacoes/1
-    def destroy
-      @acomodacao.destroy
-      if @acomodacao.destroy
-        head :no_content, status: :ok
-      else
-        render json: @acomodacao.errors, status: :unprocessable_entity
-      end
-    end
+    # def destroy
+    #   @acomodacao.destroy
+    #   if @acomodacao.destroy
+    #     head :no_content, status: :ok
+    #   else
+    #     render json: @acomodacao.errors, status: :unprocessable_entity
+    #   end
+    # end
 
     private
     # Use callbacks to share common setup or constraints between actions.
-    def set_acomodacao
-      @acomodacao = Acomodacao.find(params[:id])
-    end
+    # def set_acomodacao
+    #   @acomodacao = Acomodacao.find(params[:id])
+    # end
 
-    # Only allow a trusted parameter "white list" through.
-    def acomodacao_params
-      params.require(:acomodacao).permit(:titulo, :descricao, :capacidade, :preco, :tipo, :logradouro, :numero,
-                                         :complemento, :bairro, :cidade, :estado)
-    end
+    # # Only allow a trusted parameter "white list" through.
+    # def acomodacao_params
+    #   params.require(:acomodacao).permit(:titulo, :descricao, :capacidade, :preco, :tipo, :logradouro, :numero,
+    #                                      :complemento, :bairro, :cidade, :estado)
+    # end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
     delete :destroy_image, on: :member
   end
 
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :acomodacoes
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      resources :acomodacoes, only: [:index, :show]
+    end
+  end
 end
